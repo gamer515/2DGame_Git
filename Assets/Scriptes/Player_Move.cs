19e6097ba9307625ee46582d4923f5730d06f647
gamer515 <tyu3389@naver.com>
2025-03-13 16:58:05 +0900

    Random Image
    멈춰있을 때 랜덤으로 멈춰있는 이미지 선택

--- Assets/Scriptes/Player_Move.cs
+++ Assets/Scriptes/Player_Move.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
+    public float jump;
+    int changeImage;
     Rigidbody2D rigid;
+    Animator animator;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButton("Jump")) //점프 중복을 제거
+        {
+            rigid.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }    
     }
 
     void FixedUpdate()
@@ -16,5 +27,9 @@ public class Player_Move : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
+        if(h == 0)
+        {
+            changeImage = Random.Range(-1, 2);
+        }
     }
 }
