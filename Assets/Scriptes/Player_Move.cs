ba7242f233326217806c529fd462e730af4a793c
gamer515 <tyu3389@naver.com>
2025-03-15 22:46:02 +0900

    Branch_Work_Check
    브랜치 되는 지 확인. 중간에 branch에서 main으로 바꾸면 기존에 있던 내용이 그 전 branch로 바뀜

--- Assets/Scriptes/Player_Move.cs
+++ Assets/Scriptes/Player_Move.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
+    float Fast_Move = 3f;
+    float Base_Move = 1.5f;
+
     public float jump;
     int changeImage;
+
     Rigidbody2D rigid;
     Animator animator;
 
@@ -16,20 +20,36 @@ public class Player_Move : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetButton("Jump")) //점프 중복을 제거
-        {
-            rigid.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-        }    
+        PlayerChange();
     }
 
     void FixedUpdate()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        PlayerMove();
+    }
 
-        if(h == 0)
+    //이동
+    void PlayerMove()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h * Base_Move, ForceMode2D.Impulse);
+    }
+
+    //변신
+    void PlayerChange()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            changeImage = Random.Range(-1, 2);
+            Base_Move = Fast_Move;
+        }
+    }
+
+    //뛰기
+    void Player_Jump()
+    {
+        if (Input.GetButton("Jump")) //점프 중복을 제거
+        {
+            rigid.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
         }
     }
 }
